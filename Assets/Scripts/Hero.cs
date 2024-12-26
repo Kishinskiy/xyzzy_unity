99ac720e6b3c2e27416845de83d50050e264ce9d
Oleg Kishinskiy <threew82@gmail.com>
2024-12-26 21:54:25 +0300

    first input

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _speed;
+    private float _direction;
+  
+    public void SetDirection(float direction)
     {
-        
+        _direction = direction;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (_direction != 0)
+        {
+            var delta = _direction * _speed * Time.deltaTime;
+            var newXPosition = transform.position.x + delta;
+            transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
+        }
     }
 }
--- Assets/Scripts/HeroInputReader.cs
+++ Assets/Scripts/HeroInputReader.cs
@@ -4,5 +4,25 @@ using UnityEngine;
 
 public class HeroInputReader : MonoBehaviour
 {
+    private Hero _hero;
+    private void Awake()
+    {
+        _hero = GetComponent<Hero>();
+    }
 
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            _hero.SetDirection(-1);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            _hero.SetDirection(1);
+        }
+        else
+        {
+            _hero.SetDirection(0);
+        }
+    }
 }
