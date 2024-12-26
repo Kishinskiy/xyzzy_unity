f291891b6f26e8abdea8a7995a460c79b87e975a
Oleg Kishinskiy <threew82@gmail.com>
2024-12-27 01:07:41 +0300

    add input system

---
+++ Assets/Scripts/HeroInputReader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HeroInputReader : MonoBehaviour
+{
+    [SerializeField] private Hero _hero;
+    
+    private void OnHorizontalMovement(InputAction.CallbackContext  context) 
+    {
+        var direction = context.ReadValue<float>();
+        _hero.SetDirection(direction);
+    }
+}
