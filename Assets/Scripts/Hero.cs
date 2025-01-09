6174b02e371c43dff0bbfaac7acbe8b11ce635d8
Oleg Kishinskiy <threew82@gmail.com>
2025-01-09 11:19:06 +0300

    fix jump with gizmo sphere debug

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -6,6 +6,9 @@ public class Hero : MonoBehaviour
 {
 	[SerializeField] private float _speed;
 	[SerializeField] private float _jumpForce;
+	[SerializeField] private LayerMask _groundLayer;
+	[SerializeField] private float _groundCheckRadius;
+	[SerializeField] private Vector3 _groundCheckPositionDelta;
     
     private Vector2 _direction;
     private Rigidbody2D _rigidbody;
@@ -25,11 +28,26 @@ public class Hero : MonoBehaviour
 	    Debug.Log("Pressed Action Button!");
     }
 
+    private bool IsGrounded()
+    {
+	  var hit = Physics2D.CircleCast(transform.position + _groundCheckPositionDelta, _groundCheckRadius, Vector2.down, 1f, _groundLayer);
+	  return hit.collider != null;
+		
+    }
+
+    // DEBUG
+    private void OnDrawGizmos()
+    {
+	    Gizmos.color = IsGrounded() ? Color.green : Color.red;
+		Gizmos.DrawSphere(transform.position + _groundCheckPositionDelta, _groundCheckRadius);
+    }
+    // END DEBUG
+    
     private void FixedUpdate()
     {
 	    _rigidbody.velocity = new Vector2(_direction.x * _speed, _rigidbody.velocity.y);
 	    var isJumping = _direction.y > 0;
-	    if (isJumping)
+	    if (isJumping && IsGrounded())
 	    {
 		    _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
 	    }
