f332c254fcd40e6c1976ec799356b1f63da9d7c6
Oleg Kishinskiy <threew82@gmail.com>
2025-01-15 18:46:59 +0300

    fixes

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -49,10 +49,12 @@ public class Hero : MonoBehaviour
 		    if (IsGrounded())
 		    {
 			    _rigidbody.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
-		    } else if (_rigidbody.velocity.y > 0)
-		    {
-			    _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
-		    }
+		    } 
+		    //else if (_rigidbody.velocity.y > 0)
+		    //{
+			//    _rigidbody.velocity = new Vector2(_rigidbody.velocity.x, _rigidbody.velocity.y * 0.5f);
+		    //}
+		    // Отключил это условие: так как высота прыжка либо слишком мала: либо случайным образом отрабатывает на максимальную высоту.
 		    
 	    }
     }
