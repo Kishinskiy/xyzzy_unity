d3128ef97e0493806d26e12e974881ffa334beec
Oleg Kishinskiy <threew82@gmail.com>
2025-01-17 02:47:57 +0300

    print message when get coin

---
+++ Assets/Scripts/Components/IncraseScoreComponent.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PixelCrew.Components
+{
+    public class IncraseScoreComponent : MonoBehaviour
+    {
+        // [SerializeField] private GameObject _objectToScore;
+        public void IncraseScore()
+        {
+            Debug.Log("you get 1 coin!");
+        }
+    }
+    
+}
